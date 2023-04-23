7a582daf43aaea53b22b25576b319afa76639225
QUEAHREN <xsyooo@icloud.com>
2023-04-23 13:24:53 +0800

    Eval.py can evaluate results now

--- eval.py
+++ eval.py
@@ -8,8 +8,8 @@ if __name__ == "__main__":
     parser.add_argument('-p', '--path', type=str,
                         default='experiments/basic_sr_ffhq_210809_142238/results')
     args = parser.parse_args()
-    real_names = list(glob.glob('{}/*_hr.png'.format(args.path)))
-    fake_names = list(glob.glob('{}/*_sr.png'.format(args.path)))
+    real_names = list(glob.glob('{}/*_gt.png'.format(args.path)))
+    fake_names = list(glob.glob('{}/*_rlt.png'.format(args.path)))
 
     real_names.sort()
     fake_names.sort()
@@ -19,8 +19,8 @@ if __name__ == "__main__":
     idx = 0
     for rname, fname in zip(real_names, fake_names):
         idx += 1
-        ridx = rname.rsplit("_hr")[0]
-        fidx = rname.rsplit("_sr")[0]
+        ridx = rname.rsplit("_gt")[0].split('_')[1]
+        fidx = rname.rsplit("_rlt")[0].split('_')[1]
         assert ridx == fidx, 'Image ridx:{ridx}!=fidx:{fidx}'.format(
             ridx, fidx)
 
